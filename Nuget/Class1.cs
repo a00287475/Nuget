47604082c8b85e8d067f766bcfee3a80b4694984
a00287475 <a00287475@mycambrian.ca>
2024-12-13 21:30:43 -0500

    Add files via upload

---
+++ Nuget/Class1.cs
@@ -0,0 +1,17 @@
+﻿//This NuGet package provides a simple class, Class1, with a method print that prints a given string to the console.
+
+
+namespace Nuget
+
+{
+
+    public class Class1
+    {
+        public void print(string s)
+        {
+            Console.WriteLine(s);
+        }
+
+    }
+
+}
